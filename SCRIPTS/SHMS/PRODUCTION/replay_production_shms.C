59ddc8fa582196bd0f674a199784cc6acf6be9f0
Melanie Rehfuss <tud20704@temple.edu>
2017-08-14 15:17:09 -0400

    editing SHMS replay

--- SCRIPTS/SHMS/PRODUCTION/replay_production_shms.C
+++ SCRIPTS/SHMS/PRODUCTION/replay_production_shms.C
@@ -69,10 +69,10 @@ void replay_production_shms (Int_t RunNumber = 0, Int_t MaxEvent = 0) {
   THaGoldenTrack* gtr = new THaGoldenTrack("P.gtr", "SHMS Golden Track", "P");
   gHaPhysics->Add(gtr);
   // Add Rastered Beam Apparatus
-  THaApparatus* beam = new THcRasteredBeam("PRASTER.rb", "Rastered Beamline");
+  THaApparatus* beam = new THcRasteredBeam("P.rb", "Rastered Beamline");
   gHaApps->Add(beam);
   // Add Physics Module to calculate primary (scattered beam - usually electrons) kinematics
-  THcPrimaryKine* kin = new THcPrimaryKine("P.kin", "SHMS Single Arm Kinematics", "P", "PRASTER.rb");
+  THcPrimaryKine* kin = new THcPrimaryKine("P.kin", "SHMS Single Arm Kinematics", "P", "P.rb");
   gHaPhysics->Add(kin);
 
   // Add event handler for prestart event 125.
